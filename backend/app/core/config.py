57a79a9175850f85f653548fecb210f782c5df49
Adriano A. de Carvalho Junior <adrianoacjr@gmail.com>
2025-12-04 11:52:36 -0300

    Setup of asyncpg on FastAPI

---
+++ backend/app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
