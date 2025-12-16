419529a2c095a522acc27146b66b3e554b27e69a
Adriano A. de Carvalho Junior <adrianoacjr@gmail.com>
2025-12-16 19:06:14 -0300

    Complete implement of ai service and openai integration.

--- backend/app/infrastructure/gateways/ai_responder_gateway_openai.py
+++ backend/app/infrastructure/gateways/ai_responder_gateway_openai.py
@@ -1,10 +1,10 @@
 from openai import AsyncOpenAI
+
 from domain.interfaces.ai_responder_gateway import IAIResponderGateway
-from core.config import settings
 
 class AIResponderGatewayOpenai(IAIResponderGateway):
-    def __init__(self):
-        self.client = AsyncOpenAI(api_key=settings.OPEN_API_KEY)
+    def __init__(self, client: AsyncOpenAI):
+        self.client = client
 
     async def generate_response(self, message: str) -> str:
         response = await self.client.responses.create(
--- backend/app/presentation/dependencies/di_ai.py
+++ backend/app/presentation/dependencies/di_ai.py
@@ -1,3 +1,5 @@
+from openai import AsyncOpenAI
+
 from domain.interfaces.ai_responder_gateway import IAIResponderGateway
 
 from application.integration.generate_ai_reply import GenerateAIReply
@@ -5,5 +7,10 @@ from application.integration.generate_ai_reply import GenerateAIReply
 from infrastructure.gateways.ai_responder_gateway_openai import AIResponderGatewayOpenai
 
 class DIAI:
-    def get_ai_response_service(self) -> IAIResponderGateway:
-        return GenerateAIReply()
+    def get_ai_gateway(self, client: AsyncOpenAI) -> IAIResponderGateway:
+        return AIResponderGatewayOpenai(client)
+
+    def get_generate_ai_reply_service(self, client: AsyncOpenAI) -> GenerateAIReply:
+        return GenerateAIReply(self.get_ai_gateway(client))
+    
+di_ai = DIAI()
