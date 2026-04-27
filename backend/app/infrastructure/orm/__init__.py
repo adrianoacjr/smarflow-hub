85a1594ae5b84211daee175de9fc21afc098e4f5
Adriano A. de Carvalho Junior <adrianoacjr@gmail.com>
2026-04-27 11:11:05 -0300

    Some Alebic configurations.

--- backend/app/infrastructure/orm/__init__.py
+++ backend/app/infrastructure/orm/__init__.py
@@ -1,3 +1,4 @@
 from infrastructure.orm.customer_orm import CustomerORM
+from infrastructure.orm.conversation_orm import ConversationORM
 from infrastructure.orm.message_orm import MessageORM
 from infrastructure.orm.user_orm import UserORM
